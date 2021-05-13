4f7e535bfb7bec6301aa32145b9901281792be2f
PamelaPz <jpamelapz02@gmail.com>
2021-05-13 10:57:44 -0500

    estilos de secciones

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
 import '../assets/scss/App.scss';
 import SectionsFifty from './SectionsFifty';
 import { Container } from 'react-bootstrap';
+import SectionGray from './SectionGray';
+import SectionPlaca from './SectionPlaca';
 
 
 function App() {
   return (
     <Container fluid className="content">
       <SectionsFifty />
+      <SectionGray />
+      <SectionPlaca />
     </Container>
   );
 }
