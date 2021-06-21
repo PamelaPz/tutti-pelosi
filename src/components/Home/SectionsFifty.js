923e501120602407d90c9037d58a0ab8f19b405b
PamelaPz <jpamelapz02@gmail.com>
2021-06-21 13:56:18 -0500

    codigo css arreglando y ajustado a mobile

--- src/components/Home/SectionsFifty.js
+++ src/components/Home/SectionsFifty.js
@@ -4,7 +4,7 @@ import dog6 from '../../assets/images/dogs6.png';
 import cel1 from '../../assets/images/cel-qr.png';
 import medalla1 from '../../assets/images/medalla-qr2.png';
 import premios from '../../assets/images/producto-premios.png';
-import producto5 from '../../assets/images/producto5.png';
+import producto5 from '../../assets/images/producto6.png';
 import circle3 from '../../assets/images/circle-socio.png';
 
 import BtnOrange from './BtnOrange';
